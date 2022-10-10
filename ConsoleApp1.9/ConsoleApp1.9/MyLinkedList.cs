aaccc056a61a58f962c24eb0ca41894bec5c5c9d
Thel2022 <r.r.pendrick@gmail.com>
2022-10-11 01:53:03 +0200

    LinkedList v.1

---
+++ ConsoleApp1.9/ConsoleApp1.9/MyLinkedList.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1._9
+{
+    public class MyLinkedList<T> : IEnumerable<T>
+    {
+        NodeForMyLL<T> head;
+        NodeForMyLL<T> tail;
+        int count;
+    
+
+        public void Add (T item)
+        {
+            NodeForMyLL<T> node = new NodeForMyLL<T>(item);
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+               
+            tail = node;
+            count++;
+        }
+        public void InsertAt (T item, int index)
+        {
+            NodeForMyLL<T> node = new NodeForMyLL<T>(item);
+            NodeForMyLL<T> previous = head;
+            NodeForMyLL<T> current = head.Next;
+            int i = 0;
+
+            if (index == 0)
+            {
+                node.Next = head;
+                head = node;
+                count++;
+            }
+
+            while (i < index && current != null)
+            {
+                node.Next = current;
+                previous.Next = node;
+                
+                i++;
+            }
+            count++;
+        }
+        public NodeForMyLL<T> GetElementAt(int index)
+        {
+
+            if (index == 0)
+            {
+                return head;
+            }
+            var temp = head.Next;
+
+            int count = 1;
+
+            while (count!= index)
+
+            {
+                temp = temp.Next;
+                count++;
+            }
+            return temp;
+
+
+        }
+        public bool Remove (T item)
+        {
+            NodeForMyLL<T> current = head;
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+            if (current != null)
+            {
+                if (current.Next != null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+                else
+                {
+                    tail = current.Previous;
+                }
+                if (current.Previous != null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+                else
+                {
+                    head = current.Next;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+        public void RemoveAt (int index)
+        {
+            NodeForMyLL<T> current = head;
+            int i = 0;
+            while (i < index && current != null)
+            {
+                current = current.Next;
+                i++;
+            }
+            if (current != null)
+            {
+                if (current.Next != null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+                else
+                {
+                    tail = current.Previous;
+                }
+                if (current.Previous != null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+                else
+                {
+                    head = current.Next;
+                }
+                count--;
+            }
+            }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+  
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            NodeForMyLL<T> current = head;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerable<T> BackEnumerator()
+        {
+            NodeForMyLL<T> current = tail;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Previous;
+            }
+        }
+             
+    }
+}
---
+++ ConsoleApp1.9/ConsoleApp1.9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList<string> linkedlist = new MyLinkedList<string>();
+            linkedlist.Add("Name");
+            linkedlist.Add("Age");
+            linkedlist.Add("Sex");
+            linkedlist.Add("Profession");
+
+            Console.WriteLine("List output");
+
+            foreach (var item in linkedlist)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Write("Enter the index of the list item to be removed:");
+
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            linkedlist.RemoveAt(index);
+
+            Console.WriteLine("List output in reverse order (subject to changes):");
+
+            foreach (var t in linkedlist.BackEnumerator())
+            {
+                Console.WriteLine(t);
+            }
+
+            Console.Write("Enter the index of the list item to be added:");
+
+            int index1 = Convert.ToInt32(Console.ReadLine());
+
+            linkedlist.InsertAt("Experience", index1);
+
+            Console.WriteLine("List output (subject to changes):");
+
+            foreach (var item in linkedlist)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
