5db27110ccd5dd833c84bf0142beb9eb3c0490f4
Thel2022 <r.r.pendrick@gmail.com>
2022-10-13 21:13:28 +0300

    AfterLessonSave

--- ConsoleApp1.9/ConsoleApp1.9/Drafts.cs
+++ ConsoleApp1.9/ConsoleApp1.9/Drafts.cs
@@ -73,6 +73,276 @@ class InvalidInputExeption : Exception
         public void InvalidInputExeption(string massage) : base(massage);
     }
 
+
+        using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string str = "Some text";
+
+            Stream stream = new MemoryStream();
+            byte[] sttob = Encoding.Unicode.GetBytes(str);
+            stream.Write(sttob);
+            stream.Flush();
+            stream.Seek(0, SeekOrigin.Begin);
+
+            var buffer = new byte[stream.Length];
+            stream.Read(buffer, 0, (int)stream.Length);
+            var sqtt = Encoding.Unicode.GetString(buffer);
+            Console.WriteLine(sqtt);
+
+            stream.Close();
+            */
+        /*
+        string str = "Some text";
+
+        Stream stream = new MemoryStream();
+        byte[] sttob = Encoding.Unicode.GetBytes(str);
+        stream.Write(sttob);
+        stream.Flush();
+        stream.Seek(0, SeekOrigin.Begin);
+
+        var buffer = new byte[1024];
+        var bufferread = 1;
+        var sqtt = "";
+
+        while (bufferread > 1)
+        {
+            bufferread = stream.Read(buffer, 0, 1024);
+            sqtt = sqtt + Encoding.Unicode.GetString(buffer);
+        }
+
+        Console.WriteLine(sqtt);
+        stream.Close();
+        */
+
+        /*
+        string str = "Some text";
+
+        Stream stream = new MemoryStream();
+        var streamwriter = new StreamWriter(stream); // new BinaryWriter(stream);
+
+        streamwriter.Write(str);
+        streamwriter.Flush();
+
+        stream.Seek(0, SeekOrigin.Begin);
+
+        var streamreader = new StreamReader(stream);
+        var str1 = streamreader.ReadToEnd();
+
+        Console.WriteLine(str1);
+        stream.Close();
+        streamreader.Close();
+        streamwriter.Close();
+        */
+
+        /*
+        string str = "Some text";
+
+        var obj = new Person()
+        {
+            Age = 12
+        };
+
+        FileStream stream = File.Open("data.bin", FileMode.OpenOrCreate);
+
+        var streamwriter = new StreamWriter(stream);
+        streamwriter.Write(obj);
+        streamwriter.Flush();
+
+        stream.Seek(0, SeekOrigin.Begin);
+
+        var streamreader = new StreamReader(stream);
+        var str1 = streamreader.ReadToEnd();
+
+
+        Console.WriteLine(str1);
+        stream.Close();
+        streamreader.Close();
+        streamwriter.Close();
+
+
+    }
+}
+
+public class Person
+{
+    public int Age { get; set; }
+    private string _name;
+    public Person()
+    {
+        _name = "Dick";
+    }
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+        */
+
+        /*
+        string str = "Some text";
+
+        var obj = new Person()
+        {
+            Age = 12
+        };
+
+        FileStream stream = File.Open("person.txt", FileMode.OpenOrCreate);
+
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(stream, obj);
+
+        stream.Seek(0, SeekOrigin.Begin);
+
+        var personfromfile = bf.Deserialize(stream);
+
+        Console.WriteLine(personfromfile);
+        stream.Close();
+
+
+
+
+        var streamwriter = new StreamWriter(stream);
+        streamwriter.Write(obj);
+        streamwriter.Flush();
+
+
+
+        var streamreader = new StreamReader(stream);
+        var str1 = streamreader.ReadToEnd();
+
+
+        Console.WriteLine(str1);
+        stream.Close();
+        streamreader.Close();
+        streamwriter.Close();
+
+
+    }
+}
+[Serializable]
+public class Person
+{
+    public int Age { get; set; }
+    private string _name;
+    public Person()
+    {
+        _name = "Dick";
+    }
+    public override string ToString()
+    {
+        return &
+    }
+}
+        */
+
+        /*
+        string str = "Some text";
+
+        var obj = new Person()
+        {
+            Age = 12
+        };
+
+        FileStream stream = File.Open("person.txt", FileMode.OpenOrCreate);
+
+        XmlSerializer xs = new XmlSerializer(typeof(Person));
+
+        xs.Serialize(stream, obj);
+
+        stream.Seek(0, SeekOrigin.Begin);
+
+
+
+        var streamwriter = new StreamWriter(stream);
+        streamwriter.Write(obj);
+        streamwriter.Flush();
+
+
+
+        var streamreader = new StreamReader(stream);
+        var str1 = streamreader.ReadToEnd();
+
+        /*stream.Seek(0, SeekOrigin.Begin);
+        var des = (Person)xs.Deserialize(stream);
+        Console.WriteLine(des);
+        */
+
+
+        /*
+        Console.WriteLine(str1); //
+        stream.Close();
+        streamreader.Close(); //
+        streamwriter.Close(); //
+
+
+    }
+}
+
+public class Person
+{
+    public int Age { get; set; }
+    private string _name;
+    public Person()
+    {
+        _name = "Dick";
+    }
+    public override string ToString()
+    {
+        return $"{Age} {_name}";
+    }
+}
+        */
+        /*
+        var obj = new Person()
+        {
+            Age = 12
+        };
+
+        FileStream stream = File.Open("person.json", FileMode.OpenOrCreate);
+        var sw = new StreamWriter(stream);
+
+        JsonSerializer js = new JsonSerializer();
+        js.Serialize(sw, obj);
+            sw.Flush();
+
+            var jsstring = JsonConvert.SerializeObject(obj);
+
+        stream.Seek(0, SeekOrigin.Begin);
+
+            var streamreader = new StreamReader(stream);
+        var str1 = streamreader.ReadToEnd();
+        stream.Seek(0, SeekOrigin.Begin);
+            stream.Close();
+        }
+    public class Person
+    {
+        public int Age { get; set; }
+        private string _name;
+        public Person()
+        {
+            _name = "Dick";
+        }
+        public override string ToString()
+        {
+            return $"{Age} {_name}";
+        }
+    }
+}
+
+
         */
     }
 }
