1c3f5ea6dd990f9cbb536e02a65a75a87ef88973
Thel2022 <r.r.pendrick@gmail.com>
2022-10-24 22:19:15 +0200

    Commit2

---
+++ WebApplication1/WebApplication1/JSTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WebApplication1
+{
+    public class JSTable
+    {
+        public JSTable() { }
+
+        public JSPerson Json()
+        {
+
+            string fileName = "Data.json";
+            string jsonString = File.ReadAllText(fileName);
+            JSPerson person = JsonConvert.DeserializeObject<JSPerson>(jsonString)!;
+            return person;
+
+        }
+           
+    }
+}
